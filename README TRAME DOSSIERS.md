3ddacbf54077b752473ca2a4e465bdbee045196f
Gotzsche <pierregotzsche@gmail.com>
2019-06-25 11:13:13 +0200

    WIP

---
+++ README TRAME DOSSIERS.md
@@ -0,0 +1,21 @@
+
+
+# SRC
+
+## Controller
+> définit l'url : la route est indiquée
+> définit le code à appliquer
+
+## DataFixtures
+> données test sur base de donnée
+
+## Entity
+> création et organisation base de donnée
+
+## Migration
+> mise à jour de la base de donnée. 
+> versionning pour pouvoir revenir en arrière.
+
+## Repository
+> requêtes personnalisées 
+> utilisation du query builder pour écrire les requêtes SQL.
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,15 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Category;
 
 class CategoryController extends AbstractController
 {
     /**
- * @Route("/category/{id}", name="category_show", requirements={"id"="\d+"}, methods={"GET"})
- */
+     * @Route("/category/{id}", name="category_show")
+     */
     public function show(Category $category)
     {
         return $this->render('category/show.html.twig', [
